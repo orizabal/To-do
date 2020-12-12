2ff33e6e2bc9720fe8b4653231eef073be990ba3
Samantha Orizabal <svorizab@uwaterloo.ca>
2020-12-12 02:17:42 -0500

    Complete and functional

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -14,10 +14,48 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Your task could not be added'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/complete/<int:id>')
+def complete(id):
+    task_to_complete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_complete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Could not complete your task'
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    task_to_edit = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task_to_edit.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Could not complete your task'
+    else:
+        return render_template('update.html', task=task_to_edit)
 
 if __name__ == "__main__":
     app.run(debug=True)
